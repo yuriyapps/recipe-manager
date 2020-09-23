80c7187b54a31629c418439cd92c51509d45d431
yuriyapps <jburshtyko@gmail.com>
2020-09-23 16:09:00 -0400

    Add Ingredient button

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../recipes.service';
 
@@ -30,20 +30,41 @@ export class RecipeEditComponent implements OnInit {
     let recipeName = '';
     let recipeImgPath = '';
     let recipeDescription = '';
+    let recipeingredients = new FormArray([]);
     if(this.editMode){
       const recipe = this.recipeService.gerRecipeById(this.id);
       recipeName = recipe.name;
       recipeImgPath = recipe.imagePath;
       recipeDescription = recipe.description;
+      if(recipe['ingredients']) {
+        for (let i of recipe.ingredients){
+          recipeingredients.push(
+            new FormGroup({
+              'name': new FormControl(i.name),
+              'amount': new FormControl(i.amount)
+            })
+          )
+        }
+      }
     }
     this.recipeForm = new FormGroup({
       'name': new FormControl(recipeName),
       'imagePath': new FormControl(recipeImgPath),
-      'description': new FormControl(recipeDescription)
+      'description': new FormControl(recipeDescription),
+      'ingredients': recipeingredients
     });
   }
   onSubmit(){
     console.log(this.recipeForm);
+  }
+
+  onAddIngredient(){
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl()
+      })
+    )
   }
 
 }
